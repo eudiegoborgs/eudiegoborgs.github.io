61a450ce4a1b87bf9421a9fc7c7c867ca7f298fb
Diego Ferreira <ferreirabdiego@gmail.com>
2020-05-12 16:12:37 -0300

    Update project

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,25 @@
 import React from "react"
+import { Link } from "gatsby"
 import Layout from "../components/themes/layout"
 import SEO from "../components/organisms/seo"
+import Content from "../components/organisms/content"
+import BlogResume from "../components/organisms/blog-resume"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Content>
+      <main style={{textAlign: 'center'}}>
+        <h1>404 - NOT FOUND</h1>
+        <p>
+          Você pode não ter encontrado o que queria, mas tenho alguns posts que podem ser bem interessantes para você.
+        </p>
+      </main>
+      <BlogResume />
+      <Link to="blog">
+        Ver mais posts
+      </Link>
+    </Content>
   </Layout>
 )
 
