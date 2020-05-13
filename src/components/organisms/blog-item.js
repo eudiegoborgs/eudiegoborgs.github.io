a570ed8cb68eb28ffbca0839379eaa02325090cf
Diego Ferreira <ferreirabdiego@gmail.com>
2020-05-13 09:08:42 -0300

    Update project

--- src/components/organisms/blog-item.js
+++ src/components/organisms/blog-item.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'gatsby';
 import { css } from 'emotion';
 
-const style = `
+const style = css`
   box-shadow: 0 10px 16px 0 rgba(0,0,0,0.1),0 6px 20px 0 rgba(0,0,0,0.1);
   border-radius: 5px;
   padding: 20px 30px;
@@ -19,7 +19,7 @@ const BlogItem = ({ content }) => {
   const { frontmatter } = content;
   return (
     <React.Fragment>
-      <div className={ css`${style}` }>
+      <div className={ style }>
         <Link to={content.fields.slug} className="blog-link">
           <h3 className>{ frontmatter.title }</h3>
           <small>{ frontmatter.date }</small>
---
+++ src/pages/contato.js
@@ -0,0 +1,63 @@
+import React from "react"
+import { css } from 'emotion';
+import Layout from "../components/themes/layout"
+import SEO from "../components/organisms/seo"
+import Content from "../components/organisms/content"
+
+const style = css`
+  text-align: left;
+  input, textarea {
+    display: block;
+    width: 100%;
+    box-shadow: 0 10px 16px 0 rgba(0,0,0,0.2);
+    border-radius: 5px;
+    border: solid 1px rgba(0,0,0,0.2);
+    padding: 5px 10px;
+  }
+  .submit {
+    text-align: center;
+    button {
+      text-transform: uppercase;
+      padding: 10px 30px;
+      cursor: pointer;
+      border: solid 1px rgba(0,0,0,0.5);
+      border-radius: 5px;
+      transition: 0.5s ease all;
+      &:hover {
+        background: black;
+        color: white;
+      }
+    }
+  }
+`;
+
+
+const NotFoundPage = () => (
+  <Layout>
+    <SEO title="Conltato" />
+    <Content>
+      <main style={{textAlign: 'center'}}>
+        <h1>Contato</h1>
+        <p>
+          Você pode me mandar uma mensagem por aqui mesmo.
+        </p>
+        <form name="contact" method="POST" data-netlify="true" className={style}>
+          <p>
+            <label>Nome: <input type="text" name="name" /></label>   
+          </p>
+          <p>
+            <label>E-mail: <input type="email" name="email" /></label>
+          </p>
+          <p>
+            <label>Mensagem: <textarea name="message"></textarea></label>
+          </p>
+          <p className="submit">
+            <button type="submit">Enviar</button>
+          </p>
+        </form>
+      </main>
+    </Content>
+  </Layout>
+)
+
+export default NotFoundPage
