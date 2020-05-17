fa5bc4bf8bac7fe5f199ce6a848b1333108786ab
Diego Ferreira <ferreirabdiego@gmail.com>
2020-05-17 13:25:27 -0300

    Update project

---
+++ src/components/atoms/light-button.spec.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { render, fireEvent } from "@testing-library/react"
+import LightButton from "./light-button"
+describe("LightButton", () => {
+  it("renders correctly", () => {
+    const { getByRole } = render(<LightButton />)
+    const button = getByRole('button')
+  })
+
+  it("Click event change title", () => {
+    const { getByRole, getByTitle, container } = render(<LightButton />)
+    const icon = getByTitle('Apagar a luz')
+    expect(icon.title).toBe('Apagar a luz')
+
+    fireEvent.click(getByRole('button'))
+    expect(icon.title).toBe('Acender a luz')
+
+    fireEvent.click(getByRole('button'))
+    expect(icon.title).toBe('Apagar a luz')
+  })
+})
