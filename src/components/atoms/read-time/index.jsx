bb21fb1f17e211ce7a9638482e67666f48af19d2
Diego Ferreira <ferreirabdiego@gmail.com>
2020-05-17 17:29:08 -0300

    Update project

--- src/components/atoms/read-time/index.jsx
+++ src/components/atoms/read-time/index.jsx
@@ -5,7 +5,7 @@ import Icon from '../icon';
 const ReadTime = (props) => {
   return (
     <span role="contentinfo">
-      <Icon {...props} source="clock-o" />{`${Math.ceil(props.time)} minuto${props.time > 1 ? '' : 's'} de leitura`}
+      <Icon {...props} source="clock-o" />{`${Math.ceil(props.time)} minuto${props.time > 1 ? 's' : ''} de leitura`}
     </span>
   )
 }
