f5dea0103c9f1b11ceccf7c6f70372cd54dc009e
Diego Borges <ferreirabdiego@gmail.com>
2021-09-30 16:12:58 -0300

    Update blog-list.js

--- src/components/templates/blog-list.js
+++ src/components/templates/blog-list.js
@@ -44,7 +44,7 @@ export const BlogListQuery = graphql`
             }
           }
           frontmatter {
-            date(locale: "pt-br", formatString: "DD [de] MMMM [de] YY")
+            date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
             title
           }
         }
