89ee07cd3d190ba894288045a1ebdf86123da279
Diego Ferreira <diegoferreira@PPM-SPO-015219.local>
2023-09-11 19:35:44 -0300

    add(logger): Update library

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,6 +57,9 @@ module.exports = {
           },
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
+          {
+            resolve: `gatsby-remark-highlight-code`
+          },
         ],
       },
     },
