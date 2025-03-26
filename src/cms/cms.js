6bebb68c98745f31b649a644e8d4d7d6365ec8af
Diego Borges <ferreirabdiego@gmail.com>
2025-03-26 14:54:26 -0300

    Atualização do gatsby

--- src/cms/cms.js
+++
@@ -1,9 +0,0 @@
-import CMS from "netlify-cms-app";
-
-if (window.location.pathname === "/admin/") {
-  import("netlify-identity-widget").then((netlifyIdentity) => {
-    netlifyIdentity.init();
-  });
-}
-
-CMS.init();
