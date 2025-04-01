2e9bb523a6363500a3bd2447f85f833a56ac4e90
Diego Borges <ferreirabdiego@gmail.com>
2025-04-01 11:21:33 -0300

    Feed RSS

--- src/components/atoms/social-list.jsx
+++ src/components/atoms/social-list.jsx
@@ -1,34 +1,50 @@
 import React from 'react';
 import Icon from './icon';
 import { faGithub, faInstagram, faLinkedin, faTwitter, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faBookmark, faRss } from '@fortawesome/free-solid-svg-icons';
 
 const socialList = [
   {
+    title: 'Instagram',
     icon: faInstagram,
     link: 'https://www.instagram.com/eudiegoborgs'
   },
   {
+    title: 'Twitter',
     icon: faTwitter,
     link: 'https://twitter.com/eudiegoborgs'
   },
   {
+    title: 'LinkedIn',
     icon: faLinkedin,
     link: 'https://www.linkedin.com/in/eudiegoborgs'
   },
   {
+    title: 'GitHub',
     icon: faGithub,
     link: 'https://github.com/eudiegoborgs'
   },
   {
+    title: 'Assine meu feed RSS',
+    icon: faRss,
+    link: '/rss.xml'
+  },
+  {
+    title: 'Se inscreva no Substack',
+    icon: faBookmark,
+    link: 'https://eudiegoborgs.substack.com/'
+  },
+  {
+    title: 'Entre em contato comigo pelo WhatsApp',
     icon: faWhatsapp,
     link: 'https://wa.me/5531986883889'
-  }
+  },
 ]
 
 const SocialList = () => (
   <React.Fragment>
     { socialList.map(item => (
-      <a href={item.link} target="_blank" rel="noopener noreferrer" title={item.icon} style={{ margin: '15px 10px' }} >
+      <a href={item.link} alt={item.title} target="_blank" rel="noopener noreferrer" title={item.icon} style={{ margin: '15px 10px' }} >
         <Icon source={item.icon} />
       </a>
     ))}
